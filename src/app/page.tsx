e1d6914422d5e6473dd3be1afcab6e59b324c3c3
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-04-20 23:55:16 +0600

    update site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Calender from "@/components/Calender";
-
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
-      <h1 className="text-center mt-5 text-xl font-semibold">Calender</h1>
-      <Calender></Calender>
+      <h1>dashbord</h1>
     </div>
   );
 }
